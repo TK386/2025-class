23dee3c5de6d47a5f696dbd9f2ca10c624180f7e
TK386 <deds1659185348@gmail.com>
2025-05-28 15:59:48 +0900

    ランダム更新

--- click-number/js/scripts.js
+++ click-number/js/scripts.js
@@ -1,17 +1,23 @@
 // This file is intentionally left blank.
-for (var num = 9; num > 0; num--) {
+for (var num = Math.floor(Math.random() * 100); num > 0; num--) {
+    var randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+    var randomSize = Math.floor(Math.random() * 100);
     var elm = document.createElement("button");
     elm.innerHTML = num;
     elm.setAttribute("id", num);
-    elm.setAttribute("class", "circle");
+    elm.style.width = `${randomSize}px`;
+    elm.style.height = `${randomSize}px`;
+    elm.style.borderRadius = `${randomSize / 2}px`;
+    elm.style.backgroundColor = randomColor;
+    elm.style.position = "absolute";
     var function_name = "remove(" + num + ")"
     elm.setAttribute("onclick", function_name);
     document.getElementById("main").appendChild(elm);
 
     var left_pos = 10;
     var top_pos = 100;
-    left_pos = left_pos + Math.floor(Math.random() * 400);
-    top_pos = top_pos + Math.floor(Math.random() * 600);
+    left_pos = left_pos + Math.floor(Math.random() * 1000);
+    top_pos = top_pos + Math.floor(Math.random() * 1000);
 
     document.getElementById(num).style.left = "" + left_pos + "px";
     document.getElementById(num).style.top = "" + top_pos + "px";
